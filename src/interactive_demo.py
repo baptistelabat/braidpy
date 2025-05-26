c109ecc71e7ac56d95593a76a1280a495c11c62e
Baptiste Labat <baptiste.labat@syro.co>
2025-05-26 23:14:03 +0200

    Format

--- src/interactive_demo.py
+++ src/interactive_demo.py
@@ -1,11 +1,11 @@
-from braidpy import Braid
+
 
 def step(description, code, var_name=None):
     print("\n" + description)
     if var_name:
-        v=var_name + "="
+        v = var_name + "="
     else:
-        v=""
+        v = ""
     input(f">>>{v}{code}")
 
     try:
@@ -18,6 +18,7 @@ def step(description, code, var_name=None):
     except Exception as e:
         print("Error during execution:", e)
         return None
+
 
 print(
     """
@@ -34,7 +35,7 @@ Step 1: Move the first strand (1) over the second (2).
 This is coded as +1 for 'strand 1 over 2' with 3 strands total.
 """,
     code="Braid([+1], n_strands=3)",
-    var_name="first_upcrossing"
+    var_name="first_upcrossing",
 )
 step(
     description="""
@@ -42,7 +43,7 @@ Step 1: Move the first strand (1) over the second (2).
 This is coded as +1 for 'strand 1 over 2' with 3 strands total.
 """,
     code="print(first_upcrossing)",
-    var_name=""
+    var_name="",
 )
 
 step(
@@ -50,52 +51,52 @@ step(
 You can also draw the resulting braid
 """,
     code="Braid([+1], n_strands=3).draw()",
-    var_name="first_upcrossing"
+    var_name="first_upcrossing",
 )
 
 step(
     description="Step 2: Now move the first strand under the second (coded as -1).",
     code="Braid([-1], n_strands=3).draw()",
-    var_name="first_downcrossing"
+    var_name="first_downcrossing",
 )
 
 step(
     description="Step 3: Invert the first upcrossing (should give the same as the downcrossing).",
     code="first_upcrossing.inverse().draw()",
-    var_name="first_downcrossing_inverted"
+    var_name="first_downcrossing_inverted",
 )
 
 step(
     description="Step 4: Move the second strand (2) under the third (3).",
     code="Braid([-2], n_strands=3).draw()",
-    var_name="second_crossing"
+    var_name="second_crossing",
 )
 
 step(
     description="Step 5: Combine the upcrossing and second crossing in a list.",
     code="Braid([1, -2], n_strands=3).draw()",
-    var_name="b"
+    var_name="b",
 )
 
 step(
     description="Step 6: Combine them using the multiplication operator. Be sure to use parenthesis",
     code="(first_upcrossing * second_crossing).draw()",
-    var_name="basic_step"
+    var_name="basic_step",
 )
 
 step(
     description="Step 7: Repeat this basic step 3 times.",
     code="(basic_step * basic_step * basic_step).draw()",
-    var_name="pure_braid"
+    var_name="pure_braid",
 )
 
 step(
     description="Step 8: Use the exponentiation operator for the same result.",
     code="(basic_step ** 3).draw()",
-    var_name="pure_braid"
+    var_name="pure_braid",
 )
 step(
     description="Step 9: Now plot the resulting braid",
     code="pure_braid.plot()",
-    var_name=""
+    var_name="",
 )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,13 +3,14 @@ from braidpy.utils import int_to_superscript, int_to_subscript
 
 def test_int_to_superscript():
     # Examples:
-    assert "t" + int_to_superscript(-1) =="t⁻¹"
-    assert "t" + int_to_superscript(2) =="t²"
-    assert "t" + int_to_superscript(-12) =="t⁻¹²"
-    assert "t" + int_to_superscript(345) =="t³⁴⁵"
+    assert "t" + int_to_superscript(-1) == "t⁻¹"
+    assert "t" + int_to_superscript(2) == "t²"
+    assert "t" + int_to_superscript(-12) == "t⁻¹²"
+    assert "t" + int_to_superscript(345) == "t³⁴⁵"
+
 
 def test_int_to_subscript():
     # Examples
-    assert "x" + int_to_subscript(2) =="x₂"
-    assert "a" + int_to_subscript(-13) =="a₋₁₃"
-    assert "i" + int_to_subscript(456) =="i₄₅₆"
+    assert "x" + int_to_subscript(2) == "x₂"
+    assert "a" + int_to_subscript(-13) == "a₋₁₃"
+    assert "i" + int_to_subscript(456) == "i₄₅₆"
