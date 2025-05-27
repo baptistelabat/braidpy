418f5aede1ebb4009a9d2d2f0f4ace9c8bacf5e5
Baptiste Labat <baptiste.labat@syro.co>
2025-05-27 08:30:51 +0200

    Update test

--- tests/test_parametric_braid.py
+++ tests/test_parametric_braid.py
@@ -11,8 +11,11 @@ def test_conversion():
     b = Braid((1, 0, 0, -2), n_strands=3)
     strands = braid_to_parametric_strands(b)
     p = ParametricBraid(strands).plot()
-
-    assert p.get_positions_at(0.5) == [(0.5, 0.2, 0.5), (0.5, -0.2, 0.5), (2, 0.0, 0.5)]
+    assert p.get_positions_at(0.5) == [
+        (0.2, 0.0, 0.5),
+        (0.0, 0.0, 0.5),
+        (0.4, 0.0, 0.5),
+    ]
 
     # Sample and print one of the strands
     for pt in strands[0].sample(10):
