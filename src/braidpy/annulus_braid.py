1272afa4e72479d8454e870c3398f43571b4933d
Baptiste Labat <baptiste.labat@syro.co>
2025-06-12 21:09:22 +0200

    Preparing for annulus braid

---
+++ src/braidpy/annulus_braid.py
@@ -0,0 +1,3 @@
+# https://people.math.wisc.edu/~aekent2/annular.pdf
+# https://www.researchgate.net/publication/228456153_Cohomology_of_Artin_groups_of_type_zAn_Bn_and_applications
+# https://math.stackexchange.com/questions/4504420/braid-groups-for-ropes
