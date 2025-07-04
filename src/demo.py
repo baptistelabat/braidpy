29cff9167a1b427ebf59980dac9fc574d23308cf
Baptiste Labat <baptiste.labat@syro.co>
2025-07-04 18:26:17 +0200

    Update demo

--- src/demo.py
+++ src/demo.py
@@ -3,9 +3,30 @@ from braidpy.parametric_braid import (
     ParametricBraid,
 )
 
-b = Braid((1, -2), n_strands=3)
+# Create a braid using list of Artin's generator
+b = Braid([1, 2, -1])
+
+# Display the corresponding braid word. You should get the notation as used by Artin 'σ₁σ₂σ₁⁻¹'.
+b.format()
+
+# Display using other common notation. You should get 'abA'.
+b.format_to_notation(target="alpha")
+
+# Draw the braid in console. Alternatively use result.plot() for more advanced 2D plot.
 b.draw()
-b.plot()
-strands = (b**12).to_parametric_strands()
+
+# Perform operations. See documentation for much more features !
+result = b * b.inverse()
+
+# Draw the resulting braid in console.
+result.draw()
+
+# Plot the resulting braid.
+result.plot()
+
+# Convert to parametric braid before ploting in 3D.
+strands = result.to_parametric_strands()
+
+
 p = ParametricBraid(strands)  # .plot()
 p.plot()
