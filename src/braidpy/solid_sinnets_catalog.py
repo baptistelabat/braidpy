79afd351b8f472703563e208b33d92453d9a0776
Baptiste Labat <baptiste.labat@syro.co>
2025-11-04 23:43:51 +0100

    Add more solid sinnets

--- src/braidpy/solid_sinnets_catalog.py
+++ src/braidpy/solid_sinnets_catalog.py
@@ -17,6 +17,7 @@ abok_3043 = AshleySolidSinnet(
     initial_counts_per_space=[3, 1, 2, 2, 2, 1],
     moves=[(1, 5), (4, 2), (5, 3), (2, 6), (3, 1), (6, 4)],
 )
+
 """
 ABOK #3044
 8-strand triangle
@@ -24,4 +25,44 @@ ABOK #3044
 abok_3044 = AshleySolidSinnet(
     initial_counts_per_space=[2, 1, 1, 1, 1, 2],
     moves=[(1, 5), (6, 4), (5, 3), (4, 2), (3, 1), (2, 6)],
+)
+
+"""
+ABOK #3045
+10-strand triangle
+"""
+abok_3045 = AshleySolidSinnet(
+    initial_counts_per_space=[2, 1, 2, 2, 1, 2],
+    moves=[(1, 5), (4, 2), (3, 1), (6, 4), (5, 3), (2, 6)],
+)
+
+"""
+ABOK #3046
+13-strand triangle
+"""
+abok_3046 = AshleySolidSinnet(
+    initial_counts_per_space=[3, 2, 3, 2, 2, 1],
+    moves=[(1, 5), (2, 6), (3, 1), (4, 2), (5, 3), (6, 4)],
+)
+
+"""
+ABOK #3047
+19-strand triangle
+"""
+abok_3047 = AshleySolidSinnet(
+    initial_counts_per_space=[2, 1, 2, 1, 2, 2, 1, 2, 1, 2, 1, 2],
+    moves=[
+        (1, 9),
+        (8, 2),
+        (3, 7),
+        (6, 4),
+        (5, 1),
+        (12, 6),
+        (7, 11),
+        (10, 8),
+        (9, 5),
+        (4, 10),
+        (11, 3),
+        (2, 12),
+    ],
 )
