c74fc81dee9c6aaaaca4a2bb15c0410e483e9e80
Baptiste Labat <baptiste.labat@syro.co>
2025-11-07 19:58:31 +0100

    Correct unwanted change

--- tests/test_craft_design_online.py
+++ tests/test_craft_design_online.py
@@ -9,7 +9,7 @@ from braidpy.craft_design_online import (
 
 
 def test_load_from_file():
-    loaded_braid = BraidModel.load_from_file("tests/7strands.b3d")
+    loaded_braid = BraidModel.load_from_file("tests/abok3054.b3d")
 
     assert loaded_braid.n_strands == 17
     assert type(loaded_braid.thread_states) is list
@@ -101,7 +101,7 @@ def test_braid_model():
 
 
 def test_save_to_file():
-    loaded_braid = BraidModel.load_from_file("tests/abok3054.b3d")
+    loaded_braid = BraidModel.load_from_file("tests/5strands.b3d")
 
     # Test saving to json
     loaded_braid.save_to_file(filepath="tests/abok3054_out.json", encode=False)
