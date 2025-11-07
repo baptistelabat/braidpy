88ef449051bd50d32baca39afb6eb3ff8aea999b
Baptiste Labat <baptiste.labat@syro.co>
2025-11-07 22:19:51 +0100

    Restore test

--- tests/test_craft_design_online.py
+++ tests/test_craft_design_online.py
@@ -82,7 +82,7 @@ def test_braid_model():
 
 
 def test_save_to_file():
-    loaded_braid = BraidModel.load_from_file("tests/5strands_move_step.b3d")
+    loaded_braid = BraidModel.load_from_file("tests/abok3054.b3d")
 
     # Test saving to json
     loaded_braid.save_to_file(filepath="tests/abok3054_out.json", encode=False)
